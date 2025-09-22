2951a00f1c533a4192654c17c2af18ee4e3cbc2a
MarcosFig-dev <156377579+MarcosFig-dev@users.noreply.github.com>
2025-09-22 18:17:27 +1200

    Update 2
    Finished styling landing page, navbar, landing cards.
    Create page templates for more pages

---
+++ src/app/certificates/page.js
@@ -0,0 +1,5 @@
+function Certificates() {
+  return <h1>My Certificates</h1>;
+}
+
+export default Certificates;
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,52 @@
 import Navbar from "../components/Navbar";
 import Image from "next/image";
 import profilePic from "../assets/profileImg.jpg";
+import LandingCard from "../components/LandingCard";
+import uniProjectsImg from "../assets/autProjects.jpg";
+import certImg from "../assets/certImg.png";
+import Styles from "./LandingPage.module.css";
 
 function LandingPage() {
   return (
     <>
       <Navbar />
-      <main>
+      <main className={Styles.container}>
         <h1 className="heading">Welcome to my portfolio!</h1>
         <Image
           src={profilePic}
           alt="Profile Picture"
           width={200}
           height={200}
+          className={Styles.profilePic}
         />
-        <div className="about-me">
+        <div className={Styles.aboutme}>
           <h3 className="heading">About Me</h3>
           <p className="body-text">
-            Hi, I’m Marcos. I’m a software development student at AUT, building
-            intuitive web apps and innovative tech. Skilled in Frontend
-            development and OOP, I combine hands-on coding experience with a
-            strong teamwork and problem-solving mindset honed from years in
-            retail.
+            Hi, I’m Marcos. I’m a software development student at AUT. I am
+            skilled in Frontend development and OOP. I combine hands-on coding
+            experience with a strong teamwork and problem-solving mindset honed
+            from years in retail.
           </p>
         </div>
-        <div className="project-slideshow">
+        <div className={Styles.projectCards}>
           <h3 className="heading">Quick Projects Overview</h3>
-          {/* Slide show of projects */}
+          <div className={Styles.cardsRow}>
+            <LandingCard
+              title="Personal Projects"
+              src={profilePic}
+              link="/personal-projects"
+            />
+            <LandingCard
+              title="University Projects"
+              src={uniProjectsImg}
+              link="/university-projects"
+            />
+            <LandingCard
+              title="Certificates"
+              src={certImg}
+              link="/certificates"
+            />
+          </div>
         </div>
       </main>
     </>
